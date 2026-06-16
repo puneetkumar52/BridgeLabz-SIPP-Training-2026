5bf25b923a869f9b765db8d7e8ff0673e0b91458
PuneetKumar <p03k10@gmail.com>
2026-06-16 09:14:22 +0530

    Revert "Puneet added folder of String and Array"
    This reverts commit c142f4ebfbb9b6792a387939525983671eb8357d.

--- Arrays/Level_1/CompareCheck.java
+++
@@ -1,9 +0,0 @@
-public class CompareCheck{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] num= new int[5];
-        for(int i=0;i<5;i++){
-            if()
-        }
-    }
-}
